3b323cce0e3fed249c49a7b17a4985aa31b8a07b
fortune2422 <fortuntestccount242@gmail.com>
2025-12-12 15:33:26 +0700

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -48,4 +48,6 @@ def serve_output(filename):
 
 if __name__ == '__main__':
     os.makedirs('/home/project/output', exist_ok=True)
-    app.run(host='0.0.0.0', port=8000)
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port)
+
